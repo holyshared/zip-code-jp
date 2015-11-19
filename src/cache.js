3fea97cec0652626b7f9cbc5da91d875b8c2f2a5
holyshared <holy.shared.design@gmail.com>
2015-11-19 14:33:28 +0900

    add doc comment

--- src/cache.js
+++ src/cache.js
@@ -1,11 +1,23 @@
 import { Promise } from 'bluebird';
 
 export class CacheAdapter {
-  constructor() {
-  }
-  find(code) {
+  /**
+   * Search the dictionary from the cache
+   *
+   * @param {string} prefix
+   * @return Promise<Object>
+   */
+  find(prefix) {
     throw new Error('Please refer to the implementation to examine the cache.');
   }
+
+  /**
+   * Cache the dictionary
+   *
+   * @param {string} prefix
+   * @param {Object} dict
+   * @return Promise<void>
+   */
   store(prefix, dict) {
     throw new Error('Please refer to the implementation to examine the cache.');
   }
@@ -16,9 +28,24 @@ export class MemoryCacheAdapter extends CacheAdapter {
     super();
     this.dicts = {};
   }
-  find(code) {
+
+  /**
+   * Search the dictionary from the cache
+   *
+   * @param {string} prefix
+   * @return Promise<Object>
+   */
+  find(prefix) {
     return Promise.resolve(null);
   }
+
+  /**
+   * Cache the dictionary
+   *
+   * @param {string} prefix
+   * @param {Object} dict
+   * @return Promise<void>
+   */
   store(prefix, dict) {
     this.dicts[prefix] = dict;
     return Promise.resolve();
@@ -29,9 +56,24 @@ export class CacheManager {
   constructor(adapter) {
     this.adapter = adapter;
   }
-  find(code) {
-    return this.adapter.find(code);
+
+  /**
+   * Search the dictionary from the cache
+   *
+   * @param {string} prefix
+   * @return Promise<Object>
+   */
+  find(prefix) {
+    return this.adapter.find(prefix);
   }
+
+  /**
+   * Cache the dictionary
+   *
+   * @param {string} prefix
+   * @param {Object} dict
+   * @return Promise<void>
+   */
   store(prefix, dict) {
     return this.adapter.store(prefix, dict);
   }
