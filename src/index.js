e8749029c12e21769f7dade86b3918836606cd5c
holyshared <holy.shared.design@gmail.com>
2016-04-29 17:08:04 +0900

    Update for new dictionary format

--- src/index.js
+++ src/index.js
@@ -15,34 +15,12 @@ export const dict = {
   CacheableDictionaryLoader: CacheableDictionaryLoader
 };
 
-const PREFECTURE_DICT = [
-  null,       '北海道',   '青森県',   '岩手県',   '宮城県',
-  '秋田県',   '山形県',   '福島県',   '茨城県',   '栃木県',
-  '群馬県',   '埼玉県',   '千葉県',   '東京都',   '神奈川県',
-  '新潟県',   '富山県',   '石川県',   '福井県',   '山梨県',
-  '長野県',   '岐阜県',   '静岡県',   '愛知県',   '三重県',
-  '滋賀県',   '京都府',   '大阪府',   '兵庫県',   '奈良県',
-  '和歌山県', '鳥取県',   '島根県',   '岡山県',   '広島県',
-  '山口県',   '徳島県',   '香川県',   '愛媛県',   '高知県',
-  '福岡県',   '佐賀県',   '長崎県',   '熊本県',   '大分県',
-  '宮崎県',   '鹿児島県', '沖縄県'
-];
-
-const PROPERTIES = {
-  0: 'prefecture',
-  1: 'city',
-  2: 'area',
-  3: 'street'
+const EmptyResult = {
+  prefecture: null,
+  city: null,
+  area: null,
+  street: null
 };
-
-let result = {};
-
-Object.keys(PROPERTIES).forEach((k) => {
-  const key = PROPERTIES[k];
-  result[key] = null;
-});
-
-const emptyResult = result;
 
 const readFile = Promise.promisify(fs.readFile);
 
@@ -61,23 +39,8 @@ export class ResolvedResult {
       object.street
     );
   }
-  static fromArray(addresses) {
-    let result = ResolvedResult.emptyResult();
-
-    addresses.forEach((val, i) => {
-      const key = PROPERTIES[i];
-
-      if (i === 0) {
-        result[key] = PREFECTURE_DICT[val];
-      } else {
-        result[key] = val;
-      }
-    });
-
-    return ResolvedResult.fromObject(result);
-  }
   static emptyResult() {
-    return Object.create(emptyResult);
+    return Object.create(EmptyResult);
   }
 }
 
@@ -119,8 +82,8 @@ export default class AddressResolver {
       if (!dict[postalCode]) {
         return this.emptyResult();
       }
-      const addresses = dict[postalCode];
-      return ResolvedResult.fromArray(addresses);
+      const address = dict[postalCode];
+      return ResolvedResult.fromObject(address);
     });
   }
   emptyResult() {
